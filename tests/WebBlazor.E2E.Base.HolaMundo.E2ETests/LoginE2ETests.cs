c0117f75697dff2cf994e1df26539eac40dc8432
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-09-01 08:47:44 -0300

    iniciando repo

---
+++ tests/WebBlazor.E2E.Base.HolaMundo.E2ETests/LoginE2ETests.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Playwright;
+
+namespace WebBlazor.E2E.Base.HolaMundo.E2ETests;
+
+internal class LoginE2ETests : PageTest
+{
+    string CookieDeSesion = "MiCookie";
+
+    [SetUp]
+    async public Task Setup()
+    {
+        await Context.AddCookiesAsync(
+        [
+            new Cookie
+            {
+                Name = CookieDeSesion,
+                Value = Guid.NewGuid().ToString("n"),
+                Url = ServidorDeLaAplicacion.UrlBase
+            }
+        ]);
+    }
+
+    [Test]
+    [Description("Test de login")]
+    public async Task TestLogin()
+    {
+        // Implement your login test logic here
+        // For example, navigate to the login page, fill in credentials, and assert successful login
+    }
+}
